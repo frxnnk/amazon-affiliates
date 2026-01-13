caea732a3029c55915c9c2d155273d4d4a2331fc
frxnnk <frxnco@protonmail.com>
2026-01-13 19:53:35 -0300

    fix: Remove .vercel build artifacts from repo

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,5 +0,0 @@
-import { c, a } from "./chunks/_@astrojs-ssr-adapter_DtuljOx7.mjs";
-export {
-  c as createExports,
-  a as start
-};
--- .vercel/output/_functions/chunks/index_D3GXOACt.mjs
+++
@@ -1,6 +0,0 @@
-import "@clerk/shared/underscore";
-import { customAlphabet, urlAlphabet } from "nanoid";
-var generateSafeId = (defaultSize = 10) => customAlphabet(urlAlphabet, defaultSize)();
-export {
-  generateSafeId as g
-};
